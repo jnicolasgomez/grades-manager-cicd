352a321b0c916f77e8e33c225630c3590a5943da
jnicolasg <nicolas.gomez@optimizely.com>
2022-10-22 18:47:40 -0500

    fix lint

--- service/test_calculate_grades.py
+++ service/test_calculate_grades.py
@@ -1,8 +1,9 @@
-import pytest
 from calculateGrades import calculateFinalGrade
 
+
 def testFinalGrade():
-    assert calculateFinalGrade([3,3,3,3]) == 3
+    assert calculateFinalGrade([3, 3, 3, 3]) == 3
+
 
 def testFinalGrade2():
-    assert calculateFinalGrade([3.6,4.8,2.4,3.3]) == 3.53
+    assert calculateFinalGrade([3.6, 4.8, 2.4, 3.3]) == 3.53
